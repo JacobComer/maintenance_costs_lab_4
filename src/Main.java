56bb329e9d84c52065fec3e7da990e41517aabdb
jakob <comerjb@mail.uc.edu>
2024-09-30 15:38:37 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int winterCost = 250;
+        int springCost = 100;
+        int summerCost = 350;
+        int autumnCost = 100;
+
+        int totalCost = winterCost + springCost + summerCost + autumnCost;
+        System.out.println("the total yearly cost is " + totalCost);
+    }
+}
